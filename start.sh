c8cbb718aacd5fa2819fd14a00d4493bac8bb262
Georgy Marrero <georgymarrero@berkeley.edu>
2020-05-06 12:59:29 -0700

    Update start script

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-exec gunicorn --bind 0.0.0.0:5000 wsgi:app --max-requests 10000 --timeout 10 --keep-alive 10 --log-level info
+exec gunicorn --bind 0.0.0.0:8080 wsgi:app --max-requests 10000 --timeout 10 --keep-alive 10 --log-level info
